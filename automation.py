12a49fd5acaccb31e4b4c5dcf83dc6d0ea6f9879
ToyosatomiminoMiko <63940347+ToyosatomiminoMiko@users.noreply.github.com>
2022-10-07 21:05:01 +0800

    Update automation.py

--- automation.py
+++ automation.py
@@ -10,6 +10,34 @@ Automation
 """
 
 
+def now() -> str:  # 时间
+    return f"[{Fore.GREEN}" + time.strftime("%m-%d %H:%M:%S", time.localtime()) + f"{Fore.RESET}]"
+
+
+"""
+高亮规则：
+Fore：前景色
+Back：背景色
+Style：样式
+"""
+
+
+def warn(s) -> None:  # 警告
+    print(now(), f"[{Fore.YELLOW}WARNING{Fore.RESET}]", s)
+
+
+def info(s) -> None:  # 信息
+    print(now(), f"[{Fore.BLUE}INFO{Fore.RESET}]", s)
+
+
+def erro(s) -> None:  # 错误
+    print(now(), f"[{Fore.RED}ERROR{Fore.RESET}]", s)
+
+
+def succ(s) -> None:  # 成功
+    print(now(), f"[{Fore.GREEN}SUCCESS{Fore.RESET}]", s)
+
+
 def replace(s: str, bf: str, af: str) -> str:
     """
     替换字符:\n
@@ -44,56 +72,28 @@ def count_time(fun):
     return improved_func
 
 
-def download_image(urls: list, headers: dict, path: str) -> int:
+def download_image(url: tuple, headers: dict, path: str) -> int:
     """
     保存网络上的图片
-    - urls
+    - url:
+    1. url[0]视频id
+    2. url[1]封面url
     图片地址列表
     - headers
     请求头
     - path
     保存路径(本地)
     """
-    i = 0
-    for url in urls:
-        file_type = url.split(".")[-1]  # 解析文件类型
-        r = requests.get(url, headers)  # 获取
-        if r.status_code != 200:
-            print(f"状态码:{r.status_code}")
-        with open(f"{path}/{i}.{file_type}", "wb+") as f1:
-            f1.write(r.content)
-        i += 1
-    print(f"下载完毕(共{0}张)", i)
-    return 0
+    file_type = url[1].split(".")[-1]  # 解析文件类型
+    r = requests.get(url[1], headers)  # 获取
+    if r.status_code != 200:
+        erro(f"image [{url[0]}] download failed[{r.status_code}].")
+        return r.status_code
+    with open(f"{path}/{url[0]}.{file_type}", "wb+") as f1:
+        f1.write(r.content)
+    succ(f"image [{url[0]}] download success.")
+    return 200
 
 
 def upload_image():
     ...
-
-
-"""
-高亮规则：
-Fore：前景色
-Back：背景色
-Style：样式
-"""
-
-
-def now() -> str:  # 时间
-    return f"[{Fore.GREEN}" + time.strftime("%m-%d %H:%M:%S", time.localtime()) + f"{Fore.RESET}]"
-
-
-def warn(s) -> None:  # 警告
-    print(now(), f"{Fore.YELLOW}[WARNING]{Fore.RESET}", s)
-
-
-def info(s) -> None:  # 信息
-    print(now(), f"{Fore.BLUE}[INFO]{Fore.RESET}", s)
-
-
-def erro(s) -> None:  # 错误
-    print(now(), f"{Fore.RED}[ERROR]{Fore.RESET}", s)
-
-
-def succ(s) -> None:  # 成功 success
-    print(now(), f"{Fore.RED}[SUCCESS]{Fore.RESET}", s)
